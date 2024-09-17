330bb681b0e9c73ad8662a43e617283ac280599c
Gieb Mier <128208941+giebm@users.noreply.github.com>
2024-09-17 10:43:52 +0800

    first commit

---
+++ src/test/java/mier/project/tarot/TarotApplicationTests.java
@@ -0,0 +1,13 @@
+package mier.project.tarot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
